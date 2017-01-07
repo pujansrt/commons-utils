2355150a7502800628c0328ce26743f913271c42
Pujan Srivastava <pujansrt@gmail.com>
2017-01-07 21:58:08 +0700

    master

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # commons-utils
+
+## Available Methods for Arrays
+
+easifyarray([1,2,3,4,5][-1]);//5
+
+shuffle([1,2,3,4,5]);	//[3,2,4,1,5]
+
+randomArray(6, 10);		//[2, 8, 4, 2, 7, 8]
+
+removeItemInArray([1,2,3,4,5,6,7], 3); //[1,2,4,5,6,7]
--- src/commons-utils.ts
+++ src/commons-utils.ts
@@ -7,6 +7,23 @@ const easifyarray = (arr:any) => {
   		}
 	});
 	return proxy;
-
 }
 
+const shuffle = (arr:any) => {
+    for (let i = arr.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
+    }
+    return arr;
+}
+
+const randomArray = (length:number, max:number) => [...new Array(length)].map((_, i) => Math.round(Math.random() * max));
+
+
+const removeItemInArray = (arr:any,key:any) => {
+	var index = arr.indexOf(key, 0);
+	if (index > -1) {
+   		arr.splice(index, 1);
+	}
+	return arr;
+};
